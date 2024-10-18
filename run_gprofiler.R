a02487f306426175a5c49de1963f0c2e102f03d7
Adam Reid <ajr236@cam.ac.uk>
2024-10-18 14:05:31 +0100

    Update run_gprofiler.R

--- run_gprofiler.R
+++ run_gprofiler.R
@@ -1,8 +1,11 @@
-library(gprofiler2)
-
+# AUTHOR: Adam James Reid
+# Copyright (C) 2024 University of Cambridge
+# This program is distributed under the terms of the GNU General Public License
 
 # USAGE
 #Rscript run_gprofiler.R <gene_list> <output_file> <organism>
+
+library(gprofiler2)
 
 # test if there is at least one argument: if not, return an error
 args = commandArgs(trailingOnly=TRUE)
@@ -11,8 +14,7 @@ if (length(args) < 3) {
   stop("USAGE: Rscript run_gprofiler.R <gene_list> <output_file> <organism e.g. hsapiens>", call.=FALSE)
 } 
 
-#print(args)
-
+# Read in command line arguments
 gene_list <- read.csv(args[1], header=FALSE)
 output_file <- args[2]
 organism <- args[3]
@@ -20,9 +22,6 @@ organism <- args[3]
 # Run gProfiler (evcodes =TRUE gives us the genes associated with each term)
 gprof_res <- gost(query = gene_list$V1,
                               organism = organism, evcodes = TRUE)
-
-# Plot results
-#gostplot(KD_in_High_go_terms, capped = TRUE, interactive = TRUE)
 
 # Filter out difficult "parents" column (it is really a list)
 gprof_res <- gprof_res$result[, -which(colnames(gprof_res$result) %in% c("parents"))]
